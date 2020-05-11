65fde4708b5a253dffed76f196d415b19569bc74
Danijela <56878144+Danijela321@users.noreply.github.com>
2020-05-11 21:13:26 +0200

    beans.xml, testing och production version

---
+++ src/com/yrgo/dataaccess/EleverDataAccessTestingVersion.java
@@ -0,0 +1,43 @@
+package com.yrgo.dataaccess;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.enterprise.inject.Alternative;
+
+import com.yrgo.domain.Elever;
+
+
+@Stateless
+@Alternative
+
+public class EleverDataAccessTestingVersion implements EleverDataAccess {
+
+	@Override
+	public void insert(Elever newElever) {
+		
+	}
+
+	@Override
+	public List<Elever> findAll() {
+		
+		Elever el1 = new Elever("Jan","Jansson","Yrgo",1);
+		Elever el2 = new Elever("Sara","Blomqvist","ITH",2);
+		Elever el3= new Elever("Adam","Andersson","Teknikhogskolan",1);
+		Elever el4=new Elever("Maria", "Marisson","Yrgo",2);
+		List<Elever> elever = new ArrayList<Elever>();
+		elever.add(el1);
+		elever.add(el2);
+		elever.add(el3);
+		elever.add(el4);
+		return elever;
+	}
+
+	@Override
+	public List<Elever> findBySurname(String surname) {
+		
+		return null;
+	}
+
+}
