8eacf2e89edb02894b71d298626a2e9d1c6008cd
Danijela <56878144+Danijela321@users.noreply.github.com>
2020-05-31 13:06:35 +0200

    javadoc som separat xml file

---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.yrgo.dataaccess"},{"l":"com.yrgo.domain"},{"l":"com.yrgo.elevermanagement"},{"l":"com.yrgo.rest"}]
